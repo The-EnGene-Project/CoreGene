90cc1997df9afe2924465f3d161fc53aedf5b49d
Gabriel Augusto Gedey Porto <108944669+GabrielGedey@users.noreply.github.com>
2025-10-12 11:34:35 -0300

    Corrige detalhe no update
    Altera o parametro passado para o update de elapsed time para update_timer

--- core_gene/src/EnGene.h
+++ core_gene/src/EnGene.h
@@ -111,7 +111,7 @@ public:
                 shader::stack()->push(m_base_shader);
                 
                 if (m_user_update_func) {
-                    m_user_update_func(elapsed_time);
+                    m_user_update_func(update_timer);
                 }
 
                 shader::stack()->pop();

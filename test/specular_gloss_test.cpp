ae23dbad6f4a1bda49314d10fb27113a7046b9b6
duduardo45 <eduardoeugeniosouza@gmail.com>
2025-11-05 09:32:41 -0300

    fix(test): Enhance specular gloss shader test configuration and reenables Windows test compatibility

--- test/specular_gloss_test.cpp
+++ test/specular_gloss_test.cpp
@@ -94,18 +94,32 @@ geometry::GeometryPtr createSphere(float radius, int sectors, int stacks) {
 int main() {
     try {
         auto on_initialize = [](engene::EnGene& app) {
+            // Enable depth testing
+            glEnable(GL_DEPTH_TEST);
+            
             // Load shaders
-            auto vs_source = readShaderSource("../core_gene/shaders/specular_gloss_vertex.glsl");
-            auto fs_source = readShaderSource("../core_gene/shaders/specular_gloss_fragment.glsl");
+            auto vs_source = readShaderSource("core_gene/shaders/specular_gloss_vertex.glsl");
+            auto fs_source = readShaderSource("core_gene/shaders/specular_gloss_fragment.glsl");
             auto specularShader = shader::Shader::Make(vs_source, fs_source);
-            specularShader->Bake();
+            
+            specularShader->configureDynamicUniform<glm::mat4>("u_model", transform::current);
+            specularShader->configureDynamicUniform<uniform::detail::Sampler2D>("u_specularMap", texture::getSamplerProvider("u_specularMap"));
+            specularShader->configureDynamicUniform<uniform::detail::Sampler2D>("u_glossMap", texture::getSamplerProvider("u_glossMap"));
 
             // Create textures programmatically
             unsigned char whitePixel[] = { 255, 255, 255, 255 };
             auto specularMap = texture::Texture::Make(1, 1, whitePixel);
 
-            unsigned char greyPixel[] = { 128, 128, 128, 255 };
-            auto glossMap = texture::Texture::Make(1, 1, greyPixel);
+            // Create a 2x2 gloss texture with varied values to visualize the effect
+            // Layout: [black, dark grey]
+            //         [light grey, white]
+            unsigned char glossPixels[] = {
+                0, 0, 0, 255,       // Bottom-left: black (no gloss)
+                85, 85, 85, 255,    // Bottom-right: dark grey (low gloss)
+                170, 170, 170, 255, // Top-left: light grey (medium gloss)
+                255, 255, 255, 255  // Top-right: white (high gloss)
+            };
+            auto glossMap = texture::Texture::Make(2, 2, glossPixels);
 
             // Create sphere
             auto sphere = createSphere(1.0f, 32, 16);
@@ -115,28 +129,58 @@ int main() {
                 .with<component::PerspectiveCamera>();
 
             scene::graph()->setActiveCamera("CameraNode");
-            scene::graph()->getNodeByName("CameraNode")->payload().get<component::TransformComponent>()->getTransform()->translate(0,0,5);
+            // Position camera higher and further back to see the orbit better
+            scene::graph()->getNodeByName("CameraNode")->payload().get<component::TransformComponent>()->getTransform()->translate(0, 3, 8);
 
-
+            // Create light with proper parameters
             auto light_transform = transform::Transform::Make();
-            light_transform->translate(2,2,2);
+            light_transform->translate(2, 2, 2);
             light::PointLightParams params;
+            params.diffuse = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+            params.specular = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+            params.ambient = glm::vec4(0.1f, 0.1f, 0.1f, 1.0f);
             auto light = light::PointLight::Make(params);
 
             scene::graph()->addNode("LightNode")
                 .with<component::TransformComponent>(light_transform)
                 .with<component::LightComponent>(light, light_transform);
 
-            scene::graph()->addNode("Sphere")
-                .with<component::GeometryComponent>(sphere)
-                .with<component::ShaderComponent>(specularShader)
-                .with<component::TextureComponent>(specularMap, "u_specularMap", 0)
-                .with<component::TextureComponent>(glossMap, "u_glossMap", 1);
+            // Create orbit node that will rotate around the origin
+            auto orbit_transform = transform::Transform::Make();
+            
+            scene::graph()->addNode("OrbitNode")
+                .with<component::TransformComponent>(orbit_transform);
+            
+            // Create sphere as a child of the orbit node, translated outward
+            auto sphere_transform = transform::Transform::Make();
+            sphere_transform->translate(3, 0, 0);  // Translate sphere 3 units on X axis
+            
+            scene::graph()->buildAt("OrbitNode")
+                .addNode("Sphere")
+                    .with<component::TransformComponent>(sphere_transform)
+                    .with<component::ShaderComponent>(specularShader)
+                    .with<component::TextureComponent>(specularMap, "u_specularMap", 0)
+                    .with<component::TextureComponent>(glossMap, "u_glossMap", 1)
+                    .with<component::GeometryComponent>(sphere)
+            ;
+
+            light::manager().bindToShader(specularShader);
+            scene::graph()->getActiveCamera()->bindToShader(specularShader);
+            specularShader->Bake();
+
+            light::manager().apply();
+
+            scene::graph()->getActiveCamera()->setAspectRatio(1.0f);
         };
 
         auto on_fixed_update = [](double dt) {
-            if (auto sphere = scene::graph()->getNodeByName("Sphere")) {
-                sphere->payload().get<component::TransformComponent>()->getTransform()->rotate(0.5f * (float)dt, 0, 1, 0);
+            // Rotate the orbit node to make the sphere orbit around the center
+            if (auto orbit = scene::graph()->getNodeByName("OrbitNode")) {
+                auto transform_component = orbit->payload().get<component::TransformComponent>();
+                if (transform_component) {
+                    // Rotate 30 degrees per second around Y axis
+                    transform_component->getTransform()->rotate(30.0f * (float)dt, 0, 1, 0);
+                }
             }
         };
 
@@ -149,7 +193,7 @@ int main() {
         engene::EnGeneConfig config;
         config.title = "Specular Gloss Test";
         config.width = 800;
-        config.height = 600;
+        config.height = 800;
 
         engene::EnGene app(on_initialize, on_fixed_update, on_render, config);
         app.run();
